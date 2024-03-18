a4b039d372ed9d541b9d824b60b1ce175da1e4a8
millyanne93 <nmillyanne20@gmail.com>
2024-03-19 00:14:52 +0300

    add get and post method

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def home():
 def about():
     return render_template("about.html", title='About')
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     return render_template("register.html", title='Register', form=form)
