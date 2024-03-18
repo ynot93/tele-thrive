2ef98df81ac1c91e52e3070104ec0fc698097d22
Tony Mputhia <tonymmputhia@gmail.com>
2024-03-18 13:39:04 +0300

    Add home and about templates

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+from dotenv import load_dotenv
+
+load_dotenv()
 app = Flask(__name__)
+
+users = [
+    {
+        'first_name': "Tony",
+        'last_name': "Mputhia",
+        'date_created': "26/02/2024",
+        'appointment': "12/04/2024",
+        'age': 28
+    },
+    {
+        'first_name': "Millyanne",
+        'last_name': "Adhiambo",
+        'date_created': "28/02/2024",
+        'appointment': "17/04/2024",
+        'age': 25
+    }
+]
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return render_template("home.html", users=users)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title='About')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
